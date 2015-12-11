566a97886ad74cfa1d9d59bb00a354dd63391672
sibukixxx <sibukixxx@gmail.com>
2015-12-11 15:18:56 +0900

    Hello world

---
+++ es6/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by yuichi.takada on 2015/12/11.
+ */
+
+let foo = 'world';
+let el = document.getElementById('content');
+el.textContent = `Hello, ${foo}!`;
---
+++ js/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by yuichi.takada on 2015/12/11.
+ */
+
+"use strict";
+
+var foo = "world";
+var el = document.getElementById("content");
+el.textContent = "Hello, " + foo + "!";
